4ae71c963710a84abb126a296483f56936fd187f
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2020-05-27 00:42:50 +0300

    Bot cleaning floor

--- AI/AI/Practice/BotDirtCleaner.cs
+++ AI/AI/Practice/BotDirtCleaner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 /*
 * The goal of Artificial Intelligence is to create a rational agent (Artificial Intelligence 1.1.4). 
@@ -41,11 +42,86 @@ namespace AI
 {
     class BotDirtCleaner
     {
+        static List<Dirt> dirtList = new List<Dirt>();
+
+        struct Dirt
+        {
+            public int posR { get; }
+            public int posC { get; }
+            public bool isCleaned { get; set; }
+
+            public Dirt(int r, int c, bool clean = false)
+            {
+                posR = r;
+                posC = c;
+                isCleaned = clean;
+            }
+        }
+
+        static class BotAction
+        {
+            static public readonly string UP = "UP";
+            static public readonly string RIGHT = "RIGHT";
+            static public readonly string DOWN = "DOWN";
+            static public readonly string LEFT = "LEFT";
+            static public readonly string CLEAN = "CLEAN";
+        }
+        
+        public static void Init()
+        {
+            int[] pos = new int[2] { 0, 0 };
+            string[] board = new string[5] {
+                                            "b---d",
+                                            "-d--d",
+                                            "--dd-",
+                                            "--d--",
+                                            "----d"
+                                            };
+
+            next_move(pos[0], pos[1], board);
+        }
+
         private static void next_move(int posR, int posC, string[] board)
+        {
+            if (dirtList.Count == 0) dirtList = FindDirt(board);
+
+            if (!AreThereDirtySpots())
+            {
+                Console.WriteLine("Floor is clean my master.");
+                return;
+            }
+
+            Dirt nearest = FindNearestDirt(posR, posC, dirtList);
+
+            ShouldBotCleanOrShouldHeMove(posR, posC, nearest);
+
+            foreach (string row in board)
+            {
+                Console.WriteLine(row);
+            }
+
+            Console.WriteLine(ShouldBotCleanOrShouldHeMove(posR, posC, nearest));      // next_move(newPosR, newPosC, newBoard);
+        }
+
+        private static string ShouldBotCleanOrShouldHeMove(int botPosR, int botPosC, Dirt dirt)
+        {
+            if (botPosR == dirt.posR && botPosC == dirt.posC)
+            {
+                dirt.isCleaned = true;
+                return BotAction.CLEAN;
+            }
+            if (botPosR > dirt.posR) return BotAction.UP;
+            if (botPosR < dirt.posR) return BotAction.DOWN;
+            if (botPosC > dirt.posC) return BotAction.LEFT;
+            if (botPosC < dirt.posC) return BotAction.RIGHT;
+            return null;
+        }
+
+        static private List<Dirt> FindDirt(string[] board)
         {
             List<Dirt> dirtList = new List<Dirt>();
 
-            for (int r = 0; r < board.Length; r++) 
+            for (int r = 0; r < board.Length; r++)
             {
                 for (int c = 0; c < board[r].Length; c++)
                 {
@@ -57,48 +133,35 @@ namespace AI
                 }
             }
 
-
-            
-            for (int i = 0; i < dirtList.Count; i++)
-            {
-                Console.WriteLine(dirtList[i].posC);
-            }            
+            return dirtList;
         }
 
-        public static void Init()
+        static private Dirt FindNearestDirt(int botPosR, int botPosC, List<Dirt> dirtList)
         {
-            //string temp = Console.ReadLine();
-            //string[] position = temp.Split(' ');
-            //int[] pos = new int[2];
-            int[] pos = new int[2] { 0, 0 };
-            //string[] board = new string[5];
+            int dist = int.MaxValue;
+            Dirt nearest = dirtList[0];
 
-            string[] board = new string[5] {
-            "b---d",
-            "-d--d",
-            "--dd-",
-            "--d--",
-            "----d",
-            };
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    board[i] = Console.ReadLine();
-            //}
-            //for (int i = 0; i < 2; i++) pos[i] = Convert.ToInt32(position[i]);
-            next_move(pos[0], pos[1], board);
-        }
-
-        struct Dirt
-        {
-            public int posR { get; }
-            public int posC { get; }
-
-            public Dirt(int r, int c)
+            foreach (Dirt dirt in dirtList)
             {
-                posR = r;
-                posC = c;
+                int temp = Math.Abs(botPosR - dirt.posR) + Math.Abs(botPosC - dirt.posC);
+                if (temp < dist && !dirt.isCleaned)
+                {
+                    dist = temp;
+                    nearest = dirt;
+                }
             }
+            return nearest;
+        }
+
+        static private bool AreThereDirtySpots()
+        {
+            bool anyLeft = false;
+            foreach (Dirt d in dirtList)
+            {
+                if (!d.isCleaned) anyLeft = true;
+                break;
+            }
+            return anyLeft;
         }
     }
 }
